f5899fb8996a7667ae59e9821939c77d95641519
Carl <cboettig@gmail.com>
2023-11-23 05:08:08 +0000

    :duck: :scroll: :earth_africa:

--- tests/testthat/test-spatial.R
+++ tests/testthat/test-spatial.R
@@ -19,14 +19,21 @@ test_that("spatial", {
 
 test_that("spatial vector read", {
 
+  skip_if_not_installed("sf")
   skip_on_os("windows") # come on duckdb, support extensions on windows
   skip_if_offline() # needs to be able to load the spatial module
   skip_on_cran()
 
+  # lazy-read external data (/vsicurl/ urls work too!)
   path <- system.file("extdata/world.gpkg", package = "duckdbfs")
   x <- open_dataset(path, format = "sf")
 
+  # read into R
+  y <- x |> to_sf()
+
   expect_s3_class(x, "tbl_lazy")
   expect_s3_class(x, "tbl")
+  expect_s3_class(y, "sf")
+
 })
 
